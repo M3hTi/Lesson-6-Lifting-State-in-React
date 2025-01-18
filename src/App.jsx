7f0f13a306bc91955507223a2317a28d8a496ffb
M3hTi <mehdinasir502@yahoo.com>
2025-01-18 13:58:41 +0330

    feat: implement props to transform data from parent component to child component

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,56 @@ import * as React from 'react'
 import './App.css'
 
 function App() {
+  const products = [
+    {
+      id: 1,
+      title: 'React Book',
+      price: 45
+    },{
+      id: 2,
+      title: 'Redux Book',
+      price: 50
+    },{
+      id: 3,
+      title: 'JavaScript Book',
+      price: 30
+    },{
+      id: 4,
+      title: 'Java Book',
+      price: 20
+    },{
+      id: 5,
+      title: 'C# Book',
+      price: 15
+    }
+  ]
+
+
   return (
     <>
-      
+      < Welcome />
+      <List items={products} />
     </>
+  )
+}
+
+function Welcome() {
+  return(
+    <h1>Hello, React</h1>
+  )
+}
+
+function List(props) {
+  return(
+    <ul>
+      {props.items.map(item => {
+        return(
+          <li key={item.id}>
+            <p>{item.title} , <span>Price: {item.price.toLocaleString("en-US", {style:"currency", currency:"USD"})} </span></p>
+          </li>
+        )
+      })}
+    </ul>
   )
 }
 
